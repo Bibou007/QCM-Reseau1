2fb552a114e482ee621672afbc5812ed4c19c1e6
Just_Name <moutalabihabib2002@gmail.com>
2022-09-06 01:03:18 +0200

    Implementations des questions du qcm de reseau

--- lib/question_model.dart
+++ lib/question_model.dart
@@ -17,189 +17,152 @@ List<Question> getQuestions(){
 
   List<Question> list=[];
    list.add(
-    Question("Une fraction des individus prélevés dans un groupe d'individus faisant l'objet d'une étude statistique est: ", [
-      Answers("Echantillon", true),
-      Answers("Population", false),
-      Answers("Echantillonage", false),
-      Answers("Données", false),
+    Question("Quelle est la longueur maximale toléré d'un cable UTP de categorie 5? ", [
+      Answers("10", false),
+      Answers("50", false),
+      Answers("100", true),
+      Answers("500", false),
     ]));
+    
     list.add(
-      Question("Propriété que possèdent tous les individus d'un groupe", 
+      Question("Quel est le support de transmission qui offre le minimum d'aténuation pour un signal", 
       [
-        Answers("Echantillonage aléatoire", false),
-        Answers("Tirage", false),
-        Answers(" Fréquence", false),
-        Answers("Caractère", true),
-      ])
-    );
-    list.add(
-      Question("On considère un lot de dix machines à laver. Chaque machine a une probabilité de tomber en panne de 1%. Quelle est la loi de probabilité suivie par la variable aléatoire 'Nombre de machine en panne'? On considère qu'il y a indépendance entre les différentes machines.", 
-      [
-        Answers("Loi Binomiale ", true),
-        Answers("Loi de Poisson", false),
-        Answers("Loi Normale ", false),
-        Answers("Loi de Bernouillie", false),
+        
+        Answers("Paire de cuivre", false),
+        Answers("Cable Coaxial ", false),
+        Answers("Fibre Optique ", true),
+        
       ])
     );
   list.add(
-    Question("Une machine produit des objets dont 3% sont de mauvaise qualité. Un client reçoit un lot de 400 objets. La variable F qui a chaque echantillon de 400 objets associe le pourcentage de defectueux suit approximativement:", 
+    Question("Parmi ces denominations, laquelle cache un nom de connecteur", 
     [
-      Answers("Une loi de poisson", false),
-      Answers("Une loi Binomiale", true),
-      Answers("Une loi normale", false),
-      Answers("Une distribution d'échantillonage", false),
+      Answers("Coaxial", false),
+      Answers("ST", false),
+      Answers("RJ-11", true),
+      
+
     ]));
     list.add(
-      Question(" Parmi les propriétés suivantes définissant une loi de Bernouillie, laquelle est fausse ",
+      Question("Un cable categorie 5 est un cable:",
       [
-        Answers("La variable statistique ne peut prendre que deux valeurs", false),
-        Answers("La probabilité associée vaut soit 1 soit 0", true),
-        Answers("La variable statistique est discrète", false),
+        Answers("Cable coaxial fin", false),
+        Answers("Cable coaxial épais", false),
+        Answers("Cabl coaxial à paire torsadée", true),
        
       ])
     );
     
     
     list.add(
-      Question("On donne P(B)=0.1;P(B/A)=0.4 et P(A)=0.2. Alors P(A/B) est égale à:",
+      Question("Quel element definit l'interface principale utilisée pour une configuration initiale d'un routeur CISCO",
       [
         
-        Answers("0.5", false),
-        Answers("0.08", false),
-        Answers("0.8", true),
-        Answers("0.4", false),
+        Answers("Une interface AUX", false),
+        Answers("Une interface Ethernet", false),
+        Answers("Une interface serie", false),
+        Answers("Une interface console", true),
       ])
     );
     list.add(
-      Question("Lors du lancer d’une pièce, la probabilité d’obtenir pile est différente de celle d’obtenir face",
+      Question("Pourquoi privilégier le cablage en cuivre pour relier des batiments?",
       [
-        Answers("Faux", true),
-        Answers("Vrai", false),
-        // Answers("", true),
-        // Answers("", false),
+        Answers("Les longueurs des cables sont supérieures", true),
+        Answers("L'installation revient moins chère", false),
+        Answers("Les pertubations electromagnétiques et radioélectriques sont limités", true),
+        Answers("Les branchements durent longtemps", false),
+        Answers("Il dispose d'un potentiel de bande passante superieure", true),
       ])
     );
     list.add(
-      Question("On lance cinq fois de suite une pièce de monnaie parfaitement équilibré. La probabilité d'avoir exactement trois fois la face pile est:",
+      Question("Un administrateur reseau est chargé d'utiliser un support reseau pouvant sétendre jusqu'à 100 mètres sans faire appel à des répéteurs. Le support choisi doit etre bon marché et facile à installer. L'installation est prévue dans un batiment existant ou l'espace pour le cablage est limité. Quel type de support répondrait le mieux à ces conditions",
       [
-        Answers("1", false),
-        Answers("(1/2)^5", true),
-        Answers("(5/16)", true),
-        // Answers("", false),
+        Answers("Du cablage STP", false),
+        Answers("Du cablage à paires torsadée non blindées", true),
+        Answers("Du cablage coaxial", false),
+        Answers("De la fibre monomode", false),
+        Answers("De la fibre multimode", false),
       ])
     );
     list.add(
-      Question("X est une variable aléatoire qui suit la loi normale centré réduite alors p(2<=T<=2.24) est égale à:",
+      Question("Que signifie le terme'attenuation' en communication de données",
       [
         
-        Answers("0.9772", false),
-        Answers("0.025", false),
-        Answers("0.0103", true),
+        Answers("Perte de la force du signal à mesure que la distance augmente", true),
+        Answers("Durée de transfert du signal entre sa source et sa destination", false),
+        Answers("Fuite des signaux d'une paire de cable à l'autre", false),
+        Answers("Renforcement d'un signal par un périphérique", false),
     
       ])
     );
     list.add(
-      Question("X est une variable aléatoire qui suit la loi normale centré réduite alors p(-1.96<=T<=2) est égale à:",
+      Question("Combien d'adresses hote peuvent etre attribué sur chaqe sous-reseau si l'adresse réseau 130.68.0.0 est utilisées avec le masque de sous-reseau 255.255.248.0, NB: Utilisable",
       [
-        Answers("0.9522", true),
-        Answers("1.952", false),
-        Answers("0.9652", false),
-        // Answers("", false),
+        Answers("30", false),
+        Answers("256", false),
+        Answers("2046", true),
+        Answers("2048", false),
+        Answers("4096", false),
       ])
     );
     list.add(
-      Question("Un dé truqué numéroté de 1 à 6 suit la probabilité suivante: p(X=1)=p(X=2)=p(X=3)=0.2; p(X=4)=0.05;p(X=5)=0.15;p(X=6)=0.2",
+      Question("Quel masque de sous-reseau doit etre attribué à l'adresse reseau 192.168.32.0 pour obtenir 254 adresses d'hotes utilisables par sous-reseau?",
       [
-        Answers("p(X>3)=0.5", false),
-        Answers("p(2<=X<=5)=0.4", false),
-        Answers("p(X<=2)=0.4", true),
-        // Answers("", false),
+        Answers("255.255.0.0", false),
+        Answers("255.255.255.0", true),
+        Answers("255.255.254.0", false),
+        Answers("255.255.248.0", false),
       ])
     );
     list.add(
-      Question("On lance un dé non pipé à six faces et on s'intéresse au succès 'Obtenir le numéro 5'. On lance 18 fois ce dé et on designe par X la variable aléatoire égale au nombre de 5 obtenu, alors l'écart-type de X est:",
+      Question("Une entreprise compte scinder son reseau en sous-reseau comprenant chacun un nombre maximale de 27 hotes. Quel masque de sous-reseau faut-il employer pour heberger le nombre d'hotes necessaires et utiliser le plus grand nombre d'adresses possibles dans chaque sous-reseau?",
       [
-        Answers("√(5/6)", false),
-        Answers("√(5/2)", true),
-        Answers("√(3)", false),
-        
-        // Answers("", false),
+        Answers("255.255.255.0", false),
+        Answers("255.255.255.192", false),
+        Answers("255.255.255.224", true),
+        Answers("255.255.255.240", false),
+        Answers("255.255.255.248", false),
       ])
     );
     list.add(
-      Question("Une expérience aléatoire est :",
+      Question("Quel type de cable reseau est utilisé ente un terminal et un port console?",
       [
-        Answers("Un jeu dont on ne connaît pas les issues", false),
-        Answers("Un jeu n’ayant pas d’issue", false),
-        Answers("Une expérience ou un jeu dont les issues sont imprévisibles", true),
-        // Answers("", false),
-      ])
-    );
-    list.add(
-      Question("Si P(A)=0 ",
-      [
-        Answers("A est un évènement improbable", false),
-        Answers("A est un évènement certain", false),
-        Answers("A est un évènement nul", true),
-        Answers("A est un évènement incertain", false),
+        Answers("Un cable croisé", false),
+        Answers("Un cable droit", false),
+        Answers("Un cable console", true),
+        Answers("Un cable raccordement", false),
         
       ])
     );
     list.add(
-      Question("Une expérience au cours de laquelle, on lance un dé équilibré de 6 faces possèdent 6 issues possibles",
+      Question("Une entreprise à succursales multiples utilise l'adresse IP 196.179.110.0. Pour une gestion plus fine de ces sous-reseaux, le responsable informatique desir pourvoir affecter une adresse IP propre à chaque sous-reseau des 10 succursales. 1-De quelle classe d'adresse s'agit-il?",
       [
-        Answers(" Faux", false),
-        Answers(" Vrai", true),
-        // Answers("", true),
-        // Answers("", false),
+        Answers("Classe A", false),
+        Answers("Classe B", false),
+        Answers("Classe C", true),
+        
+        
       ])
     );
     list.add(
-      Question("On lance simultanément 3 pièces de monnaie et on note A l’évènement : « on obtient au moins une fois « pile » ». L’évènement contraire est ",
+      Question("Quel est la valeur du masque de sous réseau correspondant à ce besoin",
       [
-        Answers("On n’obtient jamais 'pile'", true),
-        Answers(" On n’obtient que des 'faces'", true),
-        Answers(" On obtient au moins 2 fois 'pile'", false),
-        // Answers("", false),
+        Answers("255.255.255.248", false),
+        Answers(" 255.255.255.240", true),
+        Answers("255.255.255.246", false),
+       
       ])
     );
     list.add(
-      Question("Une urne contient 2 boules rouges notées R 1 et R 2 et une boule noire notée N. On choisit au hasard une première boule, on la remet dans l'urne, puis on choisit une deuxième boule. On obtient ainsi un couple de boules. On suppose que chacun des couples a la même probabilité d'être obtenu. La probabilité d'obtenir 2 boules rouges est: ",
+      Question("3-Combien de machines chaque sous-reseau pourra-t-il comporter?",
       [
-        Answers("1/9", false),
-        Answers("1/6", false),
-        Answers("4/9", true),
-        Answers("1/3", false),
+        Answers("12", false),
+        Answers("14", true),
+        Answers("16", false),
+   
       ])
     );
-    
-    list.add(
-      Question("X est une variable aléatoire qui suit une loi binomiale de paramètres n et p. X est une variable aléatoire",
-      [
-        Answers("Réelle", false),
-        Answers("Continue", false),
-        Answers("Discrète", true),
-        // Answers("", false),
-      ])
-    );
-    list.add(
-      Question("X est une variable aléatoire qui suit une loi binomiale de paramètres n et p. L’espérance mathématique E(X) de X est donnée par",
-      [
-        Answers("E(X ) = npq", false),
-        Answers("E(X ) = np", true),
-        // Answers("Discrète", true),
-        // Answers("", false),
-      ])
-    );
-    list.add(
-      Question("Dans un jeu de 32 cartes, on en tire une au hasard. Toutes les cartes ont la même probabilité d'être tirées. La probabilité d'obtenir un as ou un carreau est",
-      [
-        Answers("3/8", false),
-        Answers("1/4", false),
-        Answers("11/32", true),
-        Answers("1/32", false),
-      ])
-    );
+   
 
 
 
--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,7 +1,3 @@
-// ignore_for_file: prefer_const_constructors, unused_import, avoid_web_libraries_in_flutter
-
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:quiz_app1/question_model.dart';
 
@@ -23,30 +19,30 @@ class _QuizScreenState extends State<QuizScreen> {
     // define the datas
 
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 13, 11, 112),
+      backgroundColor: const Color.fromARGB(255, 13, 11, 112),
       body: Container(
-        margin: EdgeInsets.symmetric(vertical: 16, horizontal: 35),
+        margin: const EdgeInsets.symmetric(vertical: 16, horizontal: 35),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 50.0,
               ),
-              Text(
+              const Text(
                 "QCM Stat-Proba",
                 style: TextStyle(color: Colors.white, fontSize: 25),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 50.0,
               ),
               _questionWidget(),
               _answerList(),
-              _nextButton(),
-              SizedBox(
+              // _nextButton(),
+              const SizedBox(
                 height: 50.0,
               ),
-              Text('Made with ♥ by M&H'),
+              const Text('Made with ♥ by M&H'),
               //  Text('Good luck', style: TextStyle(color: Colors.white,)),
             ],
           ),
@@ -64,22 +60,22 @@ class _QuizScreenState extends State<QuizScreen> {
           child: Text(
               "Question ${currentQuestionIndex + 1}/${questionList.length.toString()}",
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                   color: Colors.white,
                   fontSize: 18,
                   fontWeight: FontWeight.w600)),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         Container(
             alignment: Alignment.center,
-            padding: EdgeInsets.all(32),
+            padding: const EdgeInsets.all(32),
             decoration: BoxDecoration(
                 color: Colors.orangeAccent,
                 borderRadius: BorderRadius.circular(16)),
             width: double.infinity,
             child: Text(
               questionList[currentQuestionIndex].questionText,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 18,
                 fontWeight: FontWeight.w600,
@@ -101,68 +97,46 @@ class _QuizScreenState extends State<QuizScreen> {
   }
 
   Widget _answerButton(Answers answer) {
-    bool isSelected = answer == selectedAnswer;
-    return Container(
-      width: double.infinity,
-      height: 48,
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      child: ElevatedButton(
-          child: Text(answer.answerText),
-          style: ElevatedButton.styleFrom(
-            shape: StadiumBorder(),
-            primary: isSelected ? Colors.orangeAccent : Colors.white,
-            onPrimary: isSelected ? Colors.white : Colors.black,
-          ),
-          onPressed: () {
-            if (selectedAnswer == null) {
-              if (answer.isCorrect) {
-                score++;
-                isGood = true;
-              print(score);
-              }else{
-                // isGood = false;
-                print(score);
-              }
-            }
-            setState(() {
-              selectedAnswer = answer;
-            });
-          }),
-    );
-  }
-
-  _nextButton() {
     bool isLastQuestion = false;
     if (currentQuestionIndex == questionList.length - 1) {
       isLastQuestion = true;
     }
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.5,
-      height: 48,
-      child: ElevatedButton(
-          child: Text("Next"),
-          style: ElevatedButton.styleFrom(
-            shape: StadiumBorder(),
-            primary: Colors.blueAccent,
-            onPrimary: Colors.white,
-          ),
-          onPressed: () {
-            if (isLastQuestion) {
-              showDialog(context: context,
-              barrierDismissible: false,
-              builder: (_) => _showScoreDialog());
-            } else {
-              showDialog(context: context,
-              barrierDismissible: false,
-               builder: (_) => _showHelpDialog());
-              setState(() {
-                // isGood=false;
-                // selectedAnswer = null;
-                // currentQuestionIndex++;
-              });
-            }
-          }),
-    );
+    bool isSelected = answer == selectedAnswer;
+    return Container(
+        width: double.infinity,
+        height: 48,
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        child: ElevatedButton(
+            child: Text(answer.answerText),
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              primary: isSelected ? Colors.orangeAccent : Colors.white,
+              onPrimary: isSelected ? Colors.white : Colors.black,
+            ),
+            onPressed: () {
+              if (selectedAnswer == null) {
+                if (answer.isCorrect) {
+                  score++;
+                  isGood = true;
+                  // print(score);
+                }
+              }
+              ;
+              if (isLastQuestion) {
+                showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (_) => _showScoreDialog());
+              } else {
+                showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (_) => _showHelpDialog());
+                setState(() {
+                  selectedAnswer = answer;
+                });
+              }
+            }));
   }
 
   _showScoreDialog() {
@@ -205,7 +179,7 @@ class _QuizScreenState extends State<QuizScreen> {
                 isGood = false;
               });
             }),
-            child: Text('Continue'))
+            child: const Text('Continue'))
       ],
     );
   }
